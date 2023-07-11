6c773cef884d175eb4db01fde0554f69fe28acf8
Georleshia Minter <georleshiam@gmail.com>
2023-07-11 15:12:57 -0500

    update

--- c-sharp/CodeChallenge1/ArrayReverse.cs
+++ c-sharp/CodeChallenge1/ArrayReverse.cs
@@ -3,10 +3,17 @@ namespace CodeChallenges
 {
     public class CodeChallenge1
     {
-        public static int[] ReverseArray(int[] array)
-        {
-
-            return array;
-        }
+    public static int[] ReverseArray(int[] array)
+    {
+      int[] tempArray = new int[array.Length];
+      int taLength = tempArray.Length; //5
+                                       //loops help us traverse an array
+      for (int i = array.Length; i > 0; i--)
+      {
+        tempArray[taLength - i] = array[i - 1];
+        //Console.WriteLine(array[i]);
+      }
+      return tempArray;
     }
+  }
 }
